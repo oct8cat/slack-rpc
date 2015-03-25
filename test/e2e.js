ee6034769b3ccd464d15b85edf2102470518c2e0
Tako Neko <oct8cat@gmail.com>
2015-03-25 11:57:31 +0300

    Tests are now uses `.type(form)` for POST requests.

--- test/e2e.js
+++ test/e2e.js
@@ -20,21 +20,21 @@ describe('SlackRPC', function () {
     })
     describe('POST /', function () {
         it('Should deny no-trigger-word requests', function (done) {
-            agent.post('/').send(fixtures.requests[0]).expect(200, function (err, res) {
+            agent.post('/').type('form').send(fixtures.requests[0]).expect(200, function (err, res) {
                 if (err) { done(err); return }
                 res.body.text.should.match('Wat?')
                 done()
             })
         })
         it('Should deny unknown-trigger-word requests', function (done) {
-            agent.post('/').send(fixtures.requests[1]).expect(200, function (err, res) {
+            agent.post('/').type('form').send(fixtures.requests[1]).expect(200, function (err, res) {
                 if (err) { done(err); return }
                 res.body.text.should.match('Wat do with "wat do"?')
                 done()
             })
         })
         it('Should execute valid requests', function (done) {
-            agent.post('/').send(fixtures.requests[2]).expect(200, function (err, res) {
+            agent.post('/').type('form').send(fixtures.requests[2]).expect(200, function (err, res) {
                 if (err) { done(err); return }
                 res.body.text.should.match(new RegExp(process.cwd()))
                 done()
